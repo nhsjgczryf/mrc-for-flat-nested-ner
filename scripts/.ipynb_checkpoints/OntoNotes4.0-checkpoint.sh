c217141f9f3f35c467ef76d4762a193f905944eb
nhsjgczryf <wnhkcs@gmail.com>
2020-09-15 22:38:12 +0800

    remove redundant files

--- scripts/.ipynb_checkpoints/OntoNotes4.0-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2 python -m torch.distributed.launch --nproc_per_node 3 train.py \
---max_grad_norm 0.5 \
---train_span_method  gold \
---lr 8e-6 \
---max_tokens 450 \
---cls \
---span_layer \
---allow_impossible
